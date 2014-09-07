9ae69031a5bb59da01b63fdfb8f0643aac571567
Samir Bakhshi <Samir@Rohits-MacBook-Pro.local>
2014-09-07 16:44:14 -0700

    Added cmments to script

--- interest/compound_interest.rb
+++ interest/compound_interest.rb
@@ -1,23 +1,29 @@
-def raw_input(prompt= "")
+# The following function will prompt a user for input. The parameter prompt is the text that will be displayed on the screen asking for user input.
+# 'gets' will take the input from user and will make it available. The input will be stored as a string. 
+def get_raw_input(prompt= "")
     print prompt
     gets
 end
 
-x = raw_input("What is the principal?\n")
-y = raw_input("What is the interest rate per quarter?\n")
-z = raw_input("For how many quarters?\n")
+# The following variables will be defined by the end user using the get_raw_input function.
+# These variables will be later used in the interest calculation.
+principal = get_raw_input("What is the principal?\n")
+interest_rate = get_raw_input("What is the interest rate?\n")
+period = get_raw_input("For how many period(s)?\n")
 
-amount = x.to_f*(1+y.to_f/100)**z.to_f
-interest = amount-x.to_f
+# The amount variable is used to calculate the total amount due for an end user set period on an end user set principal and interest rate, compounded. 
+amount = principal.to_f*(1+interest_rate.to_f/100)**period.to_f
+# The interest variable is used to display the compounded interest calculated.
+interest = amount-principal.to_f
 
 puts ""
 puts "The total amount due for principal:"
-puts x
+puts principal
 puts "with interest rate of:"
-puts y
-puts "per quarter, for:"
-puts z
-puts "quaters, is:"
+puts interest_rate
+puts " for:"
+puts 
+puts "period(s), is:"
 puts '%.2f' % amount
 puts ""
 puts "The total interest calculated for the period is:"
--- interest/simple_interest.rb
+++ interest/simple_interest.rb
@@ -1,23 +1,28 @@
-def raw_input(prompt= "")
+# The following function will prompt a user for input. The parameter prompt is the text that will be displayed on the screen asking for user input.
+# 'gets' will take the input from user and will make it available. The input will be stored as a string. 
+def get_raw_input(prompt= "")
     print prompt
     gets
 end
 
-x = raw_input("What is the principal?\n")
-y = raw_input("What is the interet rate per quarter?\n")
-z = raw_input("For how many quarters?\n")
+# The following variables will be defined by the end user using the get_raw_input function.
+principal = get_raw_input("What is the principal?\n")
+interest_rate = get_raw_input("What is the interest rate per quarter?\n")
+period = get_raw_input("For how many period(s)?\n")
 
-interest = x.to_f*y.to_f/100*z.to_f
-amount = x.to_f+interest
+# The interest variable is used to calculate the simple interest for end user set principal, interest rate and period.
+interest = principal.to_f*interest_rate.to_f/100*period.to_f
+# The amount variable is used to calculate the total amount due after interest has been added to the prinicpal.
+amount = principal.to_f+interest
 
 puts ""
 puts "The interest calculated for principal:"
-puts x
+puts principal
 puts "with interest rate of:"
-puts y
-puts "per quarter, for:"
-puts z
-puts "is:"
+puts interest_rate
+puts "for:"
+puts period
+puts "period(s), is:"
 puts '%.2f' % interest
 puts ""
 puts "The total amount due is:"
